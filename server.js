aa223bd80a27f466858add3124c51a0b20f77f0f
gitnet51@gmail.com <gitnet51@gmail.com>
2025-07-24 13:57:57 +0200

    edit the path type to string

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/api/scrape', async (req, res) => {
     const browser = await puppeteer.launch({ 
          headless: true, // or "new" for the latest headless mode
           args: ["--no-sandbox", "--disable-setuid-sandbox"] , 
-          executablePath: puppeteer.executablePath() //|| '/usr/bin/chromium-browser' // Adjust path if necessary
+          executablePath: puppeteer.executablePath().toString() //|| '/usr/bin/chromium-browser' // Adjust path if necessary
 
     });
     const page = await browser.newPage();
